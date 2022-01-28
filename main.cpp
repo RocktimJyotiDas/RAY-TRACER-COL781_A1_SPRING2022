078e1ab644637a7c3c661affcd1ce408b2a86a9b
ROCKTIM JYOTI DAS <rocktimjyotidas@gmail.com>
2022-01-28 15:39:13 +0530

    Friday_evening_commit1

--- main.cpp
+++ main.cpp
@@ -1,89 +1,96 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <vector>
 #include <string>
+#include <vector>
 #include <cmath>
+#include <limits>
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+
 #include "Vect.h"
 #include "Ray.h"
 #include "Camera.h"
 #include "Color.h"
+#include "Source.h"
 #include "Light.h"
-#include "Sphere.h"
 #include "Object.h"
+#include "Sphere.h"
 #include "Plane.h"
 
-using namespace  std;
 
-struct RGBType{
-    double r;
-    double g;
-    double b;
+using namespace std;
+
+struct RGBType {
+	double r;
+	double g;
+	double b;
 };
 
-void savebmp(const char *filename, int w, int h, int dpi, RGBType *data){
-    FILE *f;
-    int k = w*h;
-    int s = 4*k;
-    int filesize = 54 + s;
-
-    double factor =  39.375;
-    int m =  static_cast<int>(factor);
-
-    int ppm = dpi*m;
-
-    unsigned char bmpfileheader[14] = {'B', 'M', 0,0,0,0, 0,0,0,0, 54,0,0,0};
-    unsigned char bmpinfoheader[40] = {40,0,0,0, 0,0,0,0, 0,0,0,0, 1,0,24,0};
-
-    bmpfileheader[2] = (unsigned char)(filesize);
-    bmpfileheader[3] = (unsigned char)(filesize);
-    bmpfileheader[4] = (unsigned char)(filesize);
-    bmpfileheader[5] = (unsigned char)(filesize);
-
-    bmpinfoheader[4] = (unsigned char)(w);
-    bmpinfoheader[5] = (unsigned char)(w>>8);
-    bmpinfoheader[6] = (unsigned char)(w>>16);
-    bmpinfoheader[7] = (unsigned char)(w>>24);
-
-    bmpinfoheader[8] = (unsigned char)(h);
-    bmpinfoheader[9] = (unsigned char)(h>>8);
-    bmpinfoheader[10] = (unsigned char)(h>>16);
-    bmpinfoheader[11] = (unsigned char)(h>>24);
-
-    bmpinfoheader[21] = (unsigned char)(s);
-    bmpinfoheader[22] = (unsigned char)(s>>8);
-    bmpinfoheader[23] = (unsigned char)(s>>16);
-    bmpinfoheader[24] = (unsigned char)(s>>24);
-
-    bmpinfoheader[25] = (unsigned char)(ppm);
-    bmpinfoheader[26] = (unsigned char)(ppm>>8);
-    bmpinfoheader[27] = (unsigned char)(ppm>>16);
-    bmpinfoheader[28] = (unsigned char)(ppm>>24);
-
-    bmpinfoheader[29] = (unsigned char)(ppm);
-    bmpinfoheader[30] = (unsigned char)(ppm>>8);
-    bmpinfoheader[31] = (unsigned char)(ppm>>16);
-    bmpinfoheader[32] = (unsigned char)(ppm>>24);
-
-    f = fopen(filename, "wb");
-    fwrite(bmpfileheader, 1,14, f);
-    fwrite(bmpinfoheader, 1,40,f);
-
-    for(int i = 0; i < k; i++){
-        RGBType rgb = data[i];
-
-        double red = (data[i].r)*255;
-        double green = (data[i].g)*255;
-        double blue = (data[i].b)*255;
-
-        unsigned char color[3] = {(int)floor(blue), (int)floor(green), (int)floor(red)};
-        fwrite(color, 1,3,f);
-    }
-    fclose(f);
+void savebmp (const char *filename, int w, int h, int dpi, RGBType *data) {
+	FILE *f;
+	int k = w*h;
+	int s = 4*k;
+	int filesize = 54 + s;
+	
+	double factor = 39.375;
+	int m = static_cast<int>(factor);
+	
+	int ppm = dpi*m;
+	
+	unsigned char bmpfileheader[14] = {'B','M', 0,0,0,0, 0,0,0,0, 54,0,0,0};
+	unsigned char bmpinfoheader[40] = {40,0,0,0, 0,0,0,0, 0,0,0,0, 1,0,24,0};
+	
+	bmpfileheader[ 2] = (unsigned char)(filesize);
+	bmpfileheader[ 3] = (unsigned char)(filesize>>8);
+	bmpfileheader[ 4] = (unsigned char)(filesize>>16);
+	bmpfileheader[ 5] = (unsigned char)(filesize>>24);
+	
+	bmpinfoheader[ 4] = (unsigned char)(w);
+	bmpinfoheader[ 5] = (unsigned char)(w>>8);
+	bmpinfoheader[ 6] = (unsigned char)(w>>16);
+	bmpinfoheader[ 7] = (unsigned char)(w>>24);
+	
+	bmpinfoheader[ 8] = (unsigned char)(h);
+	bmpinfoheader[ 9] = (unsigned char)(h>>8);
+	bmpinfoheader[10] = (unsigned char)(h>>16);
+	bmpinfoheader[11] = (unsigned char)(h>>24);
+	
+	bmpinfoheader[21] = (unsigned char)(s);
+	bmpinfoheader[22] = (unsigned char)(s>>8);
+	bmpinfoheader[23] = (unsigned char)(s>>16);
+	bmpinfoheader[24] = (unsigned char)(s>>24);
+	
+	bmpinfoheader[25] = (unsigned char)(ppm);
+	bmpinfoheader[26] = (unsigned char)(ppm>>8);
+	bmpinfoheader[27] = (unsigned char)(ppm>>16);
+	bmpinfoheader[28] = (unsigned char)(ppm>>24);
+	
+	bmpinfoheader[29] = (unsigned char)(ppm);
+	bmpinfoheader[30] = (unsigned char)(ppm>>8);
+	bmpinfoheader[31] = (unsigned char)(ppm>>16);
+	bmpinfoheader[32] = (unsigned char)(ppm>>24);
+	
+	f = fopen(filename,"wb");
+	
+	fwrite(bmpfileheader,1,14,f);
+	fwrite(bmpinfoheader,1,40,f);
+	
+	for (int i = 0; i < k; i++) {
+		RGBType rgb = data[i];
+		
+		double red = (data[i].r)*255;
+		double green = (data[i].g)*255;
+		double blue = (data[i].b)*255;
+		
+		unsigned char color[3] = {(int)floor(blue),(int)floor(green),(int)floor(red)};
+		
+		fwrite(color,1,3,f);
+	}
+	
+	fclose(f);
 }
 
 int winningObjectIndex(vector<double> object_intersections) {
@@ -222,8 +229,9 @@ Color getColorAt(Vect intersection_position, Vect intersecting_ray_direction, ve
 					if (secondary_intersections.at(c) <= distance_to_light_magnitude) {
 						shadowed = true;
 					}
+					break;
 				}
-				break;
+				
 			}
 			
 			if (shadowed == false) {
@@ -255,39 +263,45 @@ Color getColorAt(Vect intersection_position, Vect intersecting_ray_direction, ve
 
 int thisone;
 
-
-int main(int argc, char *arg[]){
-    cout << "rendering ..."<<endl;
-
-    int dpi  = 72;
-    int width = 640;
-    int height = 480;
-    double aspectratio = (double)width/(double)height;
-    double ambientlight = 0.2;
+int main (int argc, char *argv[]) {
+	cout << "rendering ..." << endl;
+	
+	clock_t t1, t2;
+	t1 = clock();
+	
+	int dpi = 72;
+	int width = 640;
+	int height = 480;
+	int n = width*height;
+	RGBType *pixels = new RGBType[n];
+	
+	int aadepth = 1;
+	double aathreshold = 0.1;
+	double aspectratio = (double)width/(double)height;
+	double ambientlight = 0.2;
 	double accuracy = 0.00000001;
-
-    int n = width*height;
-    RGBType *pixels = new RGBType[n];
-
-    Vect O (0,0,0);
-    Vect X(1,0,0);
-    Vect Y(0,1,0);
-    Vect Z(0,0,1);
-    Vect new_sphere_location (1.75, -0.25, 0);
-
-    Vect campos (3, 1.5, -4);
+	
+	Vect O (0,0,0);
+	Vect X (1,0,0);
+	Vect Y (0,1,0);
+	Vect Z (0,0,1);
+	
+	Vect new_sphere_location (1.75, -0.25, 0);
+	
+	Vect campos (3, 1.5, -4);
 	
 	Vect look_at (0, 0, 0);
 	Vect diff_btw (campos.getVectX() - look_at.getVectX(), campos.getVectY() - look_at.getVectY(), campos.getVectZ() - look_at.getVectZ());
-    Vect camdir = diff_btw.negative().normalize();
-    Vect camright = Y.crossProduct(camdir).normalize();
+	
+	Vect camdir = diff_btw.negative().normalize();
+	Vect camright = Y.crossProduct(camdir).normalize();
 	Vect camdown = camright.crossProduct(camdir);
-    Camera scene_cam (campos, camdir, camright, camdown);
-
-    Color white_light (1.0, 1.0, 1.0, 0);
+	Camera scene_cam (campos, camdir, camright, camdown);
+	
+	Color white_light (1.0, 1.0, 1.0, 0);
 	Color pretty_green (0.5, 1.0, 0.5, 0.3);
-    Color maroon (0.5, 0.25, 0.25, 0);
-    Color tile_floor (1, 1, 1, 2);
+	Color maroon (0.5, 0.25, 0.25, 0);
+	Color tile_floor (1, 1, 1, 2);
 	Color gray (0.5, 0.5, 0.5, 0);
 	Color black (0.0, 0.0, 0.0, 0);
 	
@@ -295,70 +309,145 @@ int main(int argc, char *arg[]){
 	Light scene_light (light_position, white_light);
 	vector<Source*> light_sources;
 	light_sources.push_back(dynamic_cast<Source*>(&scene_light));
-
-    Sphere scene_sphere (O, 1, pretty_green);
-	//Sphere scene_sphere2 (new_sphere_location, 0.5, maroon);
-    Plane scene_plane (Y, -1, maroon);
-    vector<Object*> scene_objects;
+	
+	// scene objects
+	Sphere scene_sphere (O, 1, pretty_green);
+	Sphere scene_sphere2 (new_sphere_location, 0.5, maroon);
+	Plane scene_plane (Y, -1, tile_floor);
+	vector<Object*> scene_objects;
 	scene_objects.push_back(dynamic_cast<Object*>(&scene_sphere));
-	//scene_objects.push_back(dynamic_cast<Object*>(&scene_sphere2));
+	scene_objects.push_back(dynamic_cast<Object*>(&scene_sphere2));
 	scene_objects.push_back(dynamic_cast<Object*>(&scene_plane));
-
-    double xamnt, yamnt;
-    for(int x = 0; x < width; x++){
-        for(int y = 0; y < height; y++){
-            thisone = y*width + x;
-            // start with no anti-aliasing
-            if(width > height){
-                // if image is wider than it is tall
-                xamnt = ((x+0.5)/width)*aspectratio - (((width-height)/(double)height)/2);
-                yamnt = ((height - y)+0.5)/height; 
-            }
-            else if(height > width){
-                // if image is taller than it is wide.
-                xamnt = (x+0.5)/width;
-                yamnt = (((height - y)+0.5)/height)/aspectratio - (((height - width)/(double)width)/2);
-            }
-            else{
-                xamnt = (x+0.5)/width;
-                yamnt = ((height - y)+0.5)/height;
-            }
-            Vect cam_ray_origin = scene_cam.getCameraPosition();
-            Vect cam_ray_direction = camdir.vectAdd(camright.vectMult(xamnt - 0.5).vectAdd(camdown.vectMult(yamnt - 0.5))).normalize();
-
-            Ray cam_ray(cam_ray_origin, cam_ray_direction);
-
-            vector<double> intersections;
-
-            for(int index = 0; index < scene_objects.size(); index++){
-                intersections.push_back(scene_objects.at(index)->findIntersection(cam_ray));
-            }
-
-            int index_of_winning_object = winningObjectIndex(intersections);
-            if (index_of_winning_object == -1){
-                //set background black
-                pixels[thisone].r = 0;
-                pixels[thisone].g = 0;
-                pixels[thisone].b = 0;
-            }
-            else{
-                //index corresponds to an object in our scene
-                if(intersections.at(index_of_winning_object) > accuracy){
-                    //determine the position and direction vector at the point of intersection
-                    Vect intersection_position = cam_ray_origin.vectAdd(cam_ray_direction.vectMult(intersections.at(index_of_winning_object)));
-					Vect intersecting_ray_direction = cam_ray_direction;
-                    Color this_color = getColorAt(intersection_position, intersecting_ray_direction, scene_objects, index_of_winning_object, light_sources, accuracy, ambientlight);
-                    
-                    pixels[thisone].r = this_color.getColorRed();
-                    pixels[thisone].g = this_color.getColorGreen();
-                    pixels[thisone].b = this_color.getColorBlue();
-                }
-                
-            }
-            
-        }
-    }
-    savebmp("scene.bmp", width, height, dpi, pixels);
-    return 0;
+	
+	int thisone, aa_index;
+	double xamnt, yamnt;
+	double tempRed, tempGreen, tempBlue;
+	
+	for (int x = 0; x < width; x++) {
+		for (int y = 0; y < height; y++) {
+			thisone = y*width + x;
+			
+			// start with a blank pixel
+			double tempRed[aadepth*aadepth];
+			double tempGreen[aadepth*aadepth];
+			double tempBlue[aadepth*aadepth];
+			
+			for (int aax = 0; aax < aadepth; aax++) {
+				for (int aay = 0; aay < aadepth; aay++) {
+			
+					aa_index = aay*aadepth + aax;
+					
+					srand(time(0));
+					
+					// create the ray from the camera to this pixel
+					if (aadepth == 1) {
+					
+						// start with no anti-aliasing
+						if (width > height) {
+							// the image is wider than it is tall
+							xamnt = ((x+0.5)/width)*aspectratio - (((width-height)/(double)height)/2);
+							yamnt = ((height - y) + 0.5)/height;
+						}
+						else if (height > width) {
+							// the imager is taller than it is wide
+							xamnt = (x + 0.5)/ width;
+							yamnt = (((height - y) + 0.5)/height)/aspectratio - (((height - width)/(double)width)/2);
+						}
+						else {
+							// the image is square
+							xamnt = (x + 0.5)/width;
+							yamnt = ((height - y) + 0.5)/height;
+						}
+					}
+					else {
+						// anti-aliasing
+						if (width > height) {
+							// the image is wider than it is tall
+							xamnt = ((x + (double)aax/((double)aadepth - 1))/width)*aspectratio - (((width-height)/(double)height)/2);
+							yamnt = ((height - y) + (double)aax/((double)aadepth - 1))/height;
+						}
+						else if (height > width) {
+							// the imager is taller than it is wide
+							xamnt = (x + (double)aax/((double)aadepth - 1))/ width;
+							yamnt = (((height - y) + (double)aax/((double)aadepth - 1))/height)/aspectratio - (((height - width)/(double)width)/2);
+						}
+						else {
+							// the image is square
+							xamnt = (x + (double)aax/((double)aadepth - 1))/width;
+							yamnt = ((height - y) + (double)aax/((double)aadepth - 1))/height;
+						}
+					}
+					
+					Vect cam_ray_origin = scene_cam.getCameraPosition();
+					Vect cam_ray_direction = camdir.vectAdd(camright.vectMult(xamnt - 0.5).vectAdd(camdown.vectMult(yamnt - 0.5))).normalize();
+					
+					Ray cam_ray (cam_ray_origin, cam_ray_direction);
+					
+					vector<double> intersections;
+					
+					for (int index = 0; index < scene_objects.size(); index++) {
+						intersections.push_back(scene_objects.at(index)->findIntersection(cam_ray));
+					}
+					
+					int index_of_winning_object = winningObjectIndex(intersections);
+					
+					if (index_of_winning_object == -1) {
+						// set the backgroung black
+						tempRed[aa_index] = 0;
+						tempGreen[aa_index] = 0;
+						tempBlue[aa_index] = 0;
+					}
+					else{
+						// index coresponds to an object in our scene
+						if (intersections.at(index_of_winning_object) > accuracy) {
+							// determine the position and direction vectors at the point of intersection
+							
+							Vect intersection_position = cam_ray_origin.vectAdd(cam_ray_direction.vectMult(intersections.at(index_of_winning_object)));
+							Vect intersecting_ray_direction = cam_ray_direction;
+		
+							Color intersection_color = getColorAt(intersection_position, intersecting_ray_direction, scene_objects, index_of_winning_object, light_sources, accuracy, ambientlight);
+							
+							tempRed[aa_index] = intersection_color.getColorRed();
+							tempGreen[aa_index] = intersection_color.getColorGreen();
+							tempBlue[aa_index] = intersection_color.getColorBlue();
+						}
+					}
+				}
+			}
+			
+			// average the pixel color
+			double totalRed = 0;
+			double totalGreen = 0;
+			double totalBlue = 0;
+			
+			for (int iRed = 0; iRed < aadepth*aadepth; iRed++) {
+				totalRed = totalRed + tempRed[iRed];
+			}
+			for (int iGreen = 0; iGreen < aadepth*aadepth; iGreen++) {
+				totalGreen = totalGreen + tempGreen[iGreen];
+			}
+			for (int iBlue = 0; iBlue < aadepth*aadepth; iBlue++) {
+				totalBlue = totalBlue + tempBlue[iBlue];
+			}
+			
+			double avgRed = totalRed/(aadepth*aadepth);
+			double avgGreen = totalGreen/(aadepth*aadepth);
+			double avgBlue = totalBlue/(aadepth*aadepth);
+			
+			pixels[thisone].r = avgRed;
+			pixels[thisone].g = avgGreen;
+			pixels[thisone].b = avgBlue;
+		}
+	}
+	
+	savebmp("scene_anti-aliased.bmp",width,height,dpi,pixels);
+	
+	delete pixels, tempRed, tempGreen, tempBlue;
+	
+	t2 = clock();
+	float diff = ((float)t2 - (float)t1)/1000;
+	
+	cout << diff << " seconds" << endl;
+	
+	return 0;
 }
-
